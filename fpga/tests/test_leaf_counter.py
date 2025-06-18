da170fe57b53bc39f1e70b4bf5625f29db06332b
martinloretzzz <20306567+martinloretzzz@users.noreply.github.com>
2025-06-18 16:35:51 +0200

    Add tests for leaf counter

--- fpga/tests/test_leaf_counter.py
+++ fpga/tests/test_leaf_counter.py
@@ -1,6 +1,41 @@
 import cocotb
 from cocotb.triggers import Timer
 
+
 @cocotb.test()
-async def test_leaf_counter(dut):
-    assert 1 == 1
+async def test_leaf_counter_one(dut):
+    dut.l = 0
+    dut.l[0].value = 1
+
+    await Timer(1, units="ns")
+
+    assert dut.val[0].value == 1
+
+@cocotb.test()
+async def test_leaf_counter_4(dut):
+    dut.l = 0
+    dut.l[0].value = 1
+    dut.l[20].value = 1
+    dut.l[35].value = 1
+    dut.l[39].value = 1
+
+    await Timer(1, units="ns")
+
+    assert dut.val[0].value == 4
+
+
+@cocotb.test()
+async def test_leaf_counter_multiple(dut):
+    dut.l = 0
+    dut.l[0].value = 1
+    dut.l[20].value = 1
+    dut.l[35].value = 1
+    dut.l[39].value = 1
+
+    dut.l[28].value = 1
+    dut.l[90].value = 1
+
+    await Timer(1, units="ns")
+
+    assert dut.val[0].value == 4
+    assert dut.val[3].value == 2
