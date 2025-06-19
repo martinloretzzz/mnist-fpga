7f9b5b18bb15d37a60c226f6fc51a4113c630607
martinloretzzz <20306567+martinloretzzz@users.noreply.github.com>
2025-06-19 20:33:29 +0200

    Remove MUXer from max_value_index

--- fpga/hdl/max_value_index.sv
+++ fpga/hdl/max_value_index.sv
@@ -1,22 +1,31 @@
 `timescale 1ns / 1ps
 
 module max_value_index(input logic [7:0] score [0:9], output logic [3:0] digit);
-	logic [3:0] max_l0 [0:4];
-	logic [3:0] max_l1 [0:2];
-	logic [3:0] max_l2;
+	logic [3:0] max_l0_idx [0:4];
+	logic [3:0] max_l0_val [0:4];
+	logic [3:0] max_l1_idx [0:2];
+	logic [3:0] max_l1_val [0:2];
+	logic [3:0] max_l2_idx;
+	logic [3:0] max_l2_val;
 
-	// Reduce first 8 elements
-	assign max_l0[0] = (score[0] >= score[1]) ? 4'd0 : 4'd1;
-	assign max_l0[1] = (score[2] >= score[3]) ? 4'd2 : 4'd3;
-	assign max_l0[2] = (score[4] >= score[5]) ? 4'd4 : 4'd5;
-	assign max_l0[3] = (score[6] >= score[7]) ? 4'd6 : 4'd7;
+	assign max_l0_idx[0] = (score[0] >= score[1]) ? 4'd0 : 4'd1;
+	assign max_l0_val[0] = (score[0] >= score[1]) ? score[0] : score[1];
+	assign max_l0_idx[1] = (score[2] >= score[3]) ? 4'd2 : 4'd3;
+	assign max_l0_val[1] = (score[2] >= score[3]) ? score[2] : score[3];
+	assign max_l0_idx[2] = (score[4] >= score[5]) ? 4'd4 : 4'd5;
+	assign max_l0_val[2] = (score[4] >= score[5]) ? score[4] : score[5];
+	assign max_l0_idx[3] = (score[6] >= score[7]) ? 4'd6 : 4'd7;
+	assign max_l0_val[3] = (score[6] >= score[7]) ? score[6] : score[7];
+	assign max_l0_idx[4] = (score[8] >= score[9]) ? 4'd8 : 4'd9; 
+	assign max_l0_val[4] = (score[8] >= score[9]) ? score[8] : score[9];
 
-	assign max_l1[0] = (score[max_l0[0]] >= score[max_l0[1]]) ? max_l0[0] : max_l0[1];
-	assign max_l1[1] = (score[max_l0[2]] >= score[max_l0[3]]) ? max_l0[2] : max_l0[3];
+	assign max_l1_idx[0] = (max_l0_val[0] >= max_l0_val[1]) ? max_l0_idx[0] : max_l0_idx[1];
+	assign max_l1_val[0] = (max_l0_val[0] >= max_l0_val[1]) ? max_l0_val[0] : max_l0_val[1];
+	assign max_l1_idx[1] = (max_l0_val[2] >= max_l0_val[3]) ? max_l0_idx[2] : max_l0_idx[3];
+	assign max_l1_val[1] = (max_l0_val[2] >= max_l0_val[3]) ? max_l0_val[2] : max_l0_val[3];
 
-	assign max_l2 = (score[max_l1[0]] >= score[max_l1[1]]) ? max_l1[0] : max_l1[1];
+	assign max_l2_idx = (max_l1_val[0] >= max_l1_val[1]) ? max_l1_idx[0] : max_l1_idx[1];
+	assign max_l2_val = (max_l1_val[0] >= max_l1_val[1]) ? max_l1_val[0] : max_l1_val[1];
 
-	// Combine result of first 8 with the last 2 elements
-	assign max_l0[4] = (score[8] >= score[9]) ? 4'd8 : 4'd9; 
-	assign digit = (score[max_l2] >= score[max_l0[4]]) ? max_l2 : max_l0[4];
+	assign digit = (max_l2_val >= max_l0_val[4]) ? max_l2_idx : max_l0_idx[4];
 endmodule
